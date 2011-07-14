ee4fa3395422dfed1421f918e4788158c7881b4d
Kenan Yildirim <kenanyildirim@lavabit.com>
2011-07-14 17:49:23 -0400

    Now uses GET requests, uses commas to split thousands, redid some strings

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-import random, re, urllib2, os.path, time
+import random, re, os.path, time, httplib
 from sys import exit
 
-def submitSite(query):
-        opener.open("http://pastebin.com/domain_update.php?q=" + query + "&f=1")
-        opener.close()
+def splitthousands(s, sep=','):  
+    if len(s) <= 3: return s  
+    return splitthousands(s[:-3], sep) + sep + s[-3:]
 
-print 'Pastebin Domains Center AutoSubmitter'
-print 'By KenanY'
-print 'https://github.com/KenanY'
-print '\n'
-print 'Parsing website list...'
+def submitSite(query):
+        conn = httplib.HTTPConnection("www.pastebin.com")
+        conn.request("GET", "/domain_update.php?q=" + query + "&f=1")
+        conn.close()
+
+print '''Pastebin Domains Center AutoSubmitter
+By KenanY
+https://github.com/KenanY
+\n
+Parsing website list...'''
 if os.path.isfile('top-1m.txt'):
         input = open("top-1m.txt").read()
         entries = re.split("\n+", input)
@@ -19,7 +24,6 @@ else:
     Make sure it is named "top-1m.txt" (without the quotes)
     If you don't have the list, download this file from Alexa:
         http://is.gd/AlexaList
-            ( actual URL: http://s3.amazonaws.com/alexa-static/top-1m.csv.zip )
     After that, unzip the file,
     open it in a text editor,
     and save it as a .txt file.
@@ -28,16 +32,13 @@ else:
     I'm sure you can read that fast.'''
         time.sleep(300)
         exit(0)
-print 'Creating website opener...'
-opener = urllib2.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:5.0) Gecko/20100101 Firefox/5.0')]
-print 'Done!'
-print '\n'
-print 'Time to make a choice: '
-print '[1] Randomly select which websites to submit'
-print '[2] Start from the most popular site, then work downwards'
-print '\n'
+print '''Done!
+\n
+Time to make a choice: 
+[1] Randomly select which websites to submit
+[2] Start from the most popular site, then work downwards'''
 answer = raw_input("> ")
+print '\n'
 if '1' in answer:
         print 'Randomly selecting sites!'
         print '=========='
@@ -45,7 +46,7 @@ if '1' in answer:
         while scraps < 1000000:
                 entryLine = re.split("[\W]?", entries[random.randint(1,1000000)], 1)
                 submitSite(entryLine[1])
-                print "[%s] %s (#%s)" % (str(scraps), entryLine[1], entryLine[0])
+                print "[%s] %s (#%s)" % (splitthousands(str(scraps)), entryLine[1], splitthousands(entryLine[0]))
                 scraps += 1
 elif '2' in answer:
         print 'Starting from the top sites!'
@@ -53,7 +54,7 @@ elif '2' in answer:
         for entry in entries:
                 entryLine = re.split("[\W]?", entry, 1)
                 submitSite(entryLine[1])
-                print "[%s] %s" % (str(entryLine[0]), entryLine[1])
+                print "[%s] %s" % (str(splitthousands(entryLine[0])), entryLine[1])
 else:
         print 'Learn to make choices!'
         exit(0)
