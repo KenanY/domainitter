60014f152f29fd2f9888aebebc37fa945d60abd2
Kenan Yildirim <kenanyildirim@lavabit.com>
2011-08-11 18:51:22 -0400

    Updated to follow Python's style guide ( http://www.python.org/dev/peps/pep-0008/ )

--- README.md
+++ README.md
@@ -2,19 +2,24 @@
 
 Formerly "Pastebin-Domains-Center-AutoSubmitter"
 
-This is a simple Python script which uses the list of [top one million sites according to Alexa.com][1] and submits them to [Pastebin's domains center][2].
+This is a simple Python script which uses the list of [top one million sites according to Alexa.com][1] and submits them to [Pastebin's domains center][2]. I know the accuracy of Alexa's rankings are [not][4] [so][5] [good][6], but it doesn't matter for this script. All I needed was a large list of websites (and one million is quite enough).
 
-I wrote this one night while I was bored. Guess that makes it a side project?
+I wrote this one night while I was bored. I still run it on occasion just for the heck of it.
 
 ## Usage
 
-1. Download, fork, or clone the repository
-2. If you want, get an updated list of the top 1 million websites by downloading [Alexa.com's list][3]. You'll need to unzip the archive, then convert the list to a txt file (don't change its name, or you'll break stuff).
+1. Fork [the repository][7] to get the files  
+2. If you want, get an updated list of the top one million websites by downloading [Alexa.com's list][3]. You'll need to unzip the archive, then convert the list to a plaintext file (don't change its name, or you'll break stuff).
 3. Run `domainitter.py`  
-4. Make a choice of whether you want the script to choose websites randomly to submit, or start from the top sites then work its way down
-5. ????
-6. PROFIT!! (okay, not really)
+4. Make a choice of whether you want the script to choose websites randomly to submit, or start from the top sites then work its way down  
+5. ????  
+6. PROFIT!!! (okay, not really)
 
    [1]: http://www.alexa.com/topsites (Top Sites)
    [2]: http://pastebin.com/domains (Pastebin - Domains Center)
    [3]: http://s3.amazonaws.com/alexa-static/top-1m.csv.zip (Download Alexa's list of top websites)
+   [4]: https://secure.wikimedia.org/wikipedia/en/wiki/Alexa_Internet#Accuracy_of_ranking_by_the_Alexa_Toolbar
+   [5]: http://techcrunch.com/2007/08/13/alexa-says-youtube-is-now-bigger-than-google-theyre-wrong/
+   [6]: http://techcrunch.com/2007/11/25/alexas-make-believe-internet/
+   [7]: https://github.com/KenanY/domainitter
+   
--- domainitter.py
+++ domainitter.py
@@ -15,28 +15,33 @@ Copyright (C) 2011  Kenan Yildirim
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import random, re, os.path, time, httplib
+import httplib
+import os.path
+import random
+import re
+import time
 from sys import exit
 
-def splitthousands(s, sep=','):  
+def split_thousands(s, sep=','):     # Borrowed from http://code.activestate.com/recipes/498181/#c4
     if len(s) <= 3: return s  
-    return splitthousands(s[:-3], sep) + sep + s[-3:]
+    return split_thousands(s[:-3], sep) + sep + s[-3:]
 
-def submitSite(query):
-        conn = httplib.HTTPConnection("www.pastebin.com")
-        conn.request("GET", "/domain_update.php?q=" + query + "&f=1")
-        conn.close()
+def submit_site(query):
+    conn = httplib.HTTPConnection("www.pastebin.com")
+    conn.request("GET", "/domain_update.php?q=" + query + "&f=1")
+    conn.close()
 
 print '''#####   domainitter   #####
 By KenanY
 https://github.com/KenanY
 \n
 Parsing website list...'''
+
 if os.path.isfile('top-1m.txt'):
-        input = open("top-1m.txt").read()
-        entries = re.split("\n+", input)
+    input = open("top-1m.txt").read()
+    entries = re.split("\n+", input)
 else:
-        print '''###    Error!    ###
+    print '''###    Error!    ###
     Could not find website list!
     Make sure it is named "top-1m.txt" (without the quotes)
     If you don't have the list, download this file from Alexa:
@@ -47,8 +52,9 @@ else:
     Then try re-running this script.
     The script will automatically abort in 5 minutes,
     I'm sure you can read that fast.'''
-        time.sleep(300)
-        exit(0)
+    time.sleep(300)
+    exit(0)
+    
 print '''Done!
 \n
 Time to make a choice: 
@@ -56,22 +62,23 @@ Time to make a choice:
 [2] Start from the most popular site, then work downwards'''
 answer = raw_input("> ")
 print '\n'
+
 if '1' in answer:
-        print 'Randomly selecting sites!'
-        print '=========='
-        scraps = 1
-        while scraps < 1000000:
-                entryLine = re.split("[\W]?", entries[random.randint(1,1000000)], 1)
-                submitSite(entryLine[1])
-                print "[%s] %s (#%s)" % (splitthousands(str(scraps)), entryLine[1], splitthousands(entryLine[0]))
-                scraps += 1
+    print 'Randomly selecting sites!'
+    print '=========='
+    scraps = 1
+    while scraps < 1000000:
+        entryLine = re.split("[\W]?", entries[random.randint(1,1000000)], 1)
+        submit_site(entryLine[1])
+        print "[%s] %s (#%s)" % (split_thousands(str(scraps)), entryLine[1], split_thousands(entryLine[0]))
+        scraps += 1
 elif '2' in answer:
-        print 'Starting from the top sites!'
-        print '=========='
-        for entry in entries:
-                entryLine = re.split("[\W]?", entry, 1)
-                submitSite(entryLine[1])
-                print "[%s] %s" % (str(splitthousands(entryLine[0])), entryLine[1])
+    print 'Starting from the top sites!'
+    print '=========='
+    for entry in entries:
+        entryLine = re.split("[\W]?", entry, 1)
+        submit_site(entryLine[1])
+        print "[%s] %s" % (str(split_thousands(entryLine[0])), entryLine[1])
 else:
-        print 'Learn to make choices!'
-        exit(0)
+    print 'Learn to make choices!'
+    exit(0)
