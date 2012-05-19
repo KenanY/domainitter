5f24eb7f66f85778bd8b697954c61147687314f6
Kenan Yildirim <kenanyildirim@lavabit.com>
2012-05-19 13:28:44 -0400

    Update copyright

--- domainitter.py
+++ domainitter.py
@@ -6,7 +6,7 @@
 * Licensed under the MIT:
 * http://www.opensource.org/licenses/mit-license.php
 *
-* Copyright (c) 2011, Kenan Yildirim
+* Copyright (c) 2011-2012, Kenan Yildirim
 """
 
 import os.path
